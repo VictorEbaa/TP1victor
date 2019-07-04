e641d6eace622de0336120f348190249d1dc8b0d
morridoOwO <48958253+morridoOwO@users.noreply.github.com>
2019-07-04 10:06:25 -0300

    Create Atividade de Java.java

---
+++ Atividade de Java.java
@@ -0,0 +1,18 @@
+public class Ciee{
+	public class Cliente() {
+		boolean cpf;
+		String nome;
+		String sexo;
+		String estadocivil;
+		String email;
+		int numero;
+	}
+	public class endereço() {
+		int cep;
+		String cidade;
+		String Estado;
+		String logradouro;
+		int numerocasa;
+		String bairro;
+	}
+}
